d5bf66ea1f231e380a2c69f36063eca589251266
DogecoinDark <justinvalo@yahoo.com>
2015-09-21 13:17:10 -0400

    Create onionseed.h

---
+++ src/onionseed.h
@@ -0,0 +1,18 @@
+
+#ifndef RODS_ONIONSEED_H
+#define RODS_ONIONSEED_H
+
+// hidden service seeds
+static const char *strMainNetOnionSeed[][1] = {
+    {"bmxto4gcy5u7pwic.onion"},
+    {"7gpe3lp3jpiufd54.onion"},
+    {NULL}
+};
+
+static const char *strTestNetOnionSeed[][1] = {
+    {"bmxto4gcy5u7pwic.onion"},
+    {"7gpe3lp3jpiufd54.onion"},
+    {NULL}
+};
+
+#endif

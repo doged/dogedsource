e732036168891e547f89711b5dfeccd436659796
DogecoinDark <justinvalo@yahoo.com>
2015-10-01 15:49:24 -0400

    Update bitcoingui.cpp

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -223,10 +223,10 @@ void BitcoinGUI::createActions()
     chatAction->setCheckable(true);
     tabGroup->addAction(chatAction);
     
-    radioAction = new QAction(QIcon(":/icons/radio"), tr("&Radio"), this);
-    radioAction->setToolTip(tr("listen to DOGED radio"));
-    radioAction->setCheckable(true);
-    tabGroup->addAction(radioAction);
+    DemoPlayerAction = new QAction(QIcon(":/icons/radio"), tr("&Radio"), this);
+    DemoPlayerAction->setToolTip(tr("listen to DOGED radio"));
+    DemoPlayerAction->setCheckable(true);
+    tabGroup->addAction(DemoPlayerAction);
 
     sendCoinsAction = new QAction(QIcon(":/icons/send"), tr("&Send coins"), this);
     sendCoinsAction->setToolTip(tr("Send coins to a DogeCoinDark address"));
